ef8cc1d3cc32fe976e1dda6cb7d6e7e84fc18668
raineyii <138646727+raineyii@users.noreply.github.com>
2024-07-09 12:07:49 +0800

    Dual-post when session end

---
+++ commands/host/endsession.js
@@ -0,0 +1,27 @@
+module.exports = {
+  name: 'endsession',
+  description: `End the session, sets Nepabella's status to normal! Host only.`,
+  aliases: ['sessionend', 'es', 'end', 'sessionover', 'over'],
+  args: true,
+  execute(message, args) {
+
+    if(message.member.roles.cache.some(r => r.name === "Host")) {
+      if (!args.length && args) {
+        message.client.user.setActivity(null);
+        message.channel.send(`
+<@&1258690632491204608> ended the session! 
+    Hope everyone enjoyed!`);
+      } else
+        message.client.user.setActivity(null);
+        message.channel.send(`
+<@&1258690632491204608> ended the session! 
+   Hope everyone enjoyed!
+${args.join(' ')}`);
+  
+    }
+  }
+}
+
+
+
+// message.client.user.user.setStatus('online');
--- commands/host/invcode.js
+++ commands/host/invcode.js
@@ -1,25 +1,29 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
 module.exports = {
   name: 'callinv',
   description: 'Call the invite code! Host only.',
   aliases: ['i', 'code', 'icode', 'inv', 'ic'],
-  cooldown: 3,
   args: true,
+  cooldown: -1800,
   execute(message, args) {
 
-    if(!message.member.roles.cache.some(r => r.name === "Host")) {
+    if(message.member.roles.cache.some(r => r.name === "Host")) {
       if (!args.length && args) {
         return
-      } else 
-
-
-
-        // var invCode = (`${message.content}`);
+      } 
+      
+      else 
+        message.client.user.setPresence({ activity: { name: 'Stardew Valley' }, status: 'online' });
         message.channel.send(`
 <@&1258690632491204608> declared an invite code!
     <@&1256217332376928378>, ${args.join(' ')}`
       )
+  
     }
-  },
-};
+  }
+}
+  
 
 // ${args.join(' ')}
