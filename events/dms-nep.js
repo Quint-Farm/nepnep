a86aa4a0ddf3fa5d66adc260ed5186aabe4eb3d9
raineyii <138646727+raineyii@users.noreply.github.com>
2024-07-09 09:38:55 +0800

    Update dms-nep.js
    - Fix dms checker
    - Image viewer
    - Experimental fetch when offline.

--- events/dms-nep.js
+++ events/dms-nep.js
@@ -1,14 +1,21 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
 module.exports = {
-	name: 'dms',
-	execute() {
+	name: 'message',
+	execute(message) {
 
-        const dms = client.channels.cache.get('1258628125479407616');
+        const dms = message.client.channels.cache.get('1258628125479407616');
+
 
         if (message.channel.type === 'dm') {
             dms.send(message.author.tag + ` ` + message.author.avatarURL() + `: ` + message.content);
-        }
-
+            message.channel.messages.fetch().then(messages => {
+                messages.forEach(msg => dms.send(msg.content));
+             });
+            if(message.attachments.size > 0) dms.send(message.attachments.first().url)
+            if(!message.content)
+                return;
+            {
+                
+        } 
     }
+}
 }
