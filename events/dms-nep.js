417c5e2cf390e0cc38ee6a74c3f72f8644b9bf4f
raineyii <138646727+raineyii@users.noreply.github.com>
2024-07-09 10:13:57 +0800

    Disable fetching DMS offline

--- events/dms-nep.js
+++ events/dms-nep.js
@@ -1,21 +1,15 @@
+const { DMChannel } = require("discord.js");
+
 module.exports = {
 	name: 'message',
 	execute(message) {
 
+        let beenRunning;
         const dms = message.client.channels.cache.get('1258628125479407616');
-
 
         if (message.channel.type === 'dm') {
             dms.send(message.author.tag + ` ` + message.author.avatarURL() + `: ` + message.content);
-            message.channel.messages.fetch().then(messages => {
-                messages.forEach(msg => dms.send(msg.content));
-             });
             if(message.attachments.size > 0) dms.send(message.attachments.first().url)
-            if(!message.content)
-                return;
-            {
-                
         } 
     }
-}
 }
