47167abc7f55ad923cc4fe72fd13aae9c2c93c97
raineyii <138646727+raineyii@users.noreply.github.com>
2024-07-09 13:47:46 +0800

    Update cosmetics.js

--- events/cosmetics.js
+++ events/cosmetics.js
@@ -1,19 +1,28 @@
 module.exports = {
     name: 'message',
     execute(message) {
+
+        const reactions = [null, "🐱", "🐟"]
+        const messages = ["Nya?", "Nyan?", "Meow!", "Meow", "meow", "*<@&1258314300658024531> runs away from you.*"]
+            let randomMessage = messages[Math.floor(Math.random() * messages.length)];
+            let randomReaction
     
-        if (message.content === "PM me") {
+        if (message.content === "PM me Nepabella") {
      	    message.author.send("Meow!")
         }
 
-        if (message.content === "<@&1258314300658024531>") {
-	        message.channel.send("Nya?")
-	        message.react("🐟")
-        }
-
-        if (message.content === "<@1258261514842804276>") {
+        if (message.content.includes("<@&1258314300658024531>") && !message.author.bot) {
             message.channel.send("Meow!")
             message.react("🐱")
+        }
+
+        if (message.content.includes("<@1258261514842804276>") && !message.author.bot) {
+            randomReaction = reactions[Math.floor(Math.random() * reactions.length)];
+	        message.channel.send(randomMessage)
+            if (randomReaction === null) {
+                return
+            } else message.react(randomReaction)
+	        
         }
 
     }
